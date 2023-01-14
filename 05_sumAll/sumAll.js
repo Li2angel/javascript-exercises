0320bc7b5f2776848b54b2baa8aef1e692f24dad
Li2angel <mbaalade99@gmail.com>
2023-01-14 23:44:15 +0100

    Solved the sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1,num2) {
+    let output = 0;
+    let start = num1;
+    let end = num2;
+    if(num1 > num2){
+      start = num2;
+      end = num1;
+    }
+    if(num1 < 0 || num2 < 0){
+      return "ERROR";
+    }
+    else if(typeof(num1) !== typeof(num2)){
+      return "ERROR";
+    }
+    else{
+      for(let i = start; i <= end; i++){
+        output += i;
+      }
+    }
+    return output;
+  };
+  
 
 // Do not edit below this line
 module.exports = sumAll;
