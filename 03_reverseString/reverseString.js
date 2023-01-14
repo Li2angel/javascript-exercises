b70906a98b66401dde42724411c10780fc76ae39
Li2angel <mbaalade99@gmail.com>
2023-01-14 21:18:05 +0100

    Solved the reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+const reverseString = function(text) {
+    let output = "";
+    for(let i = text.length - 1; i >= 0; i--){
+      output += text[i];
+    }
+    return output;
+  };
 
 // Do not edit below this line
 module.exports = reverseString;
